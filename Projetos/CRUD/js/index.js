9456024e3ccc86ae2f66565374b4a99800fb3e47
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2024-09-01 01:35:11 -0300

    Função formsEditSubmit() Concluída.

--- Projetos/CRUD/js/index.js
+++ Projetos/CRUD/js/index.js
@@ -1,7 +1,6 @@
 import { NovaToDo } from "./modules/classes.js";
 const list = document.querySelector('.list')
 
-
 const events = {
    formNovaToDoSubmit: function(){
       const formNewTodo = document.querySelector('.novo-item-form');
@@ -32,14 +31,23 @@ const events = {
       })
    },
    formsEditSubmit: function(){
-      const submtBtns = document.querySelectorAll('.item-form');
-      submtBtns.forEach(btn => {
-         btn.addEventListener('click', (e) =>{
+      const formToDoSubmit = document.querySelectorAll('.item-form');
+      formToDoSubmit.forEach(form => {
+         form.addEventListener('submit', (e) =>{
             e.preventDefault();
             const parentId = e.target.getAttribute(['data-parent-id']);
-            updateToDo.update(parentId)
+            
+            const formData = new FormData(form);
+            const texto = Object.fromEntries(formData)['texto'];
+            
+            //pega apenas o id do item sem o "P".
+            updateToDo.update(texto, parentId.slice(1,));
+
          });   
       });
+   },
+   checkboxClick: function(){
+
    },
    callAddEvents: function(){
       this.editFormClick();
@@ -52,7 +60,7 @@ events.formNovaToDoSubmit();
 const createTodo = {
    instanciarToDo: function(texto){
       const ToDo = new NovaToDo(texto, this.dataHoje(), false);
-      this.inserirTodoNaDB(ToDo)
+      this.inserirTodoNaDB(ToDo);
    },
    dataHoje: function(){
       return new Date().toLocaleDateString(
@@ -87,7 +95,7 @@ const loadToDos = {
    pegarToDos: function(){
       return  fetch('http://localhost:3000/list')
          .then(response => response.json())
-         .catch(error => console.log(error))
+         .catch(error => console.log(error));
    },
    inserirNoDOM: async function(ToDo){
       
@@ -95,10 +103,9 @@ const loadToDos = {
 
       //Instacia cada item da lista para que tenham os getters e setters
       //de NovaTodo
-      let ToDos =
-          data.map(item => 
+      let ToDos = data.map(item => 
             new NovaToDo(item.texto, item.data, item.status, item.id)
-         )
+         );
       if(ToDo){
          ToDos = ToDo;
       }
@@ -108,14 +115,14 @@ const loadToDos = {
          novoItem.setAttribute('id', `P${item.getId()}`)
          const compontent = `
             <div class="item-checkbox">
-               <input type="checkbox" ${item.getStatus()} id="check-C${item.getId()}" class="hidden">
+               <input type="checkbox" ${item.getStatus()} id="check-C${item.getId()}" class="hidden" data-parent-id="P${item.getId()}">
                <label for="check-C${item.getId()}" class="checkbox-label"></label>
             </div>
             <div class="item-content">
                <p class="item-texto">
                ${item.getTexto()}
                </p>
-               <form class="item-form hidden">
+               <form class="item-form hidden" data-parent-id="P${item.getId()}">
                   <input name="texto" type="text" value="${item.getTexto()}" class="item-form-texto">
                   <button type="submit" class="item-submit-form">
                      Alterar
@@ -139,8 +146,23 @@ const loadToDos = {
 }
 
 const updateToDo = {
-   update: function(parentId){
-      
+   update: async function(texto, parentId){
+      await fetch(`http://localhost:3000/list/${parentId}`,
+         {
+            method: 'PUT',
+            headers: {
+               'Content-Type': 'application/json; charst=UTF-8'
+            },
+            body: JSON.stringify(
+               {
+                  texto: `${texto}`
+               }
+            )
+         }
+      )
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.log(error))
    }
 }
 
